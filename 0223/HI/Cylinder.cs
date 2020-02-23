c61f677cf2bdda3a2e66e7f622c57ca94637df47
khi1601 <50444928+khi1601@users.noreply.github.com>
2020-02-23 15:15:38 +0900

    2주차 스터디

---
+++ 0223/HI/Cylinder.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cylinder : MonoBehaviour
+{
+    float timecount = 0;
+   // public int count = 5;
+    
+    // Start is called before the first frame update
+    void OnTriggerEnter(Collider col)
+    {
+        
+        if(col.gameObject.name=="Ball")
+        {
+            
+            Destroy(gameObject);
+            GameObject.Find("GameManager").SendMessage("coincount");
+            GameObject ball = GameObject.Find("Ball");
+            ball.GetComponent<Rigidbody>().AddForce(Vector3.back*20);
+            ball.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+            ball.transform.position = new Vector3(0, 0.5f, -3);
+            ball.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotationZ;
+            ball.GetComponent<Rigidbody>().constraints=RigidbodyConstraints.None;
+                
+
+
+
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ 0223/HI/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public int count = 5;
+    public Text unitText;
+    // Start is called before the first frame update
+    void coincount()
+    {
+        count--;
+        unitText.text = "남은 개수: " + count + "개";
+    }
+    void Start()
+    {
+        unitText.text = "남은 개수: " + count + "개";
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
